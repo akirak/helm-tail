f18370797b82a206cf4bdb5cc7703afa60aca10b
Akira Komamura <akira.komamura@gmail.com>
2018-06-11 22:55:35 +0900

    Fix commentary

--- helm-tail.el
+++ helm-tail.el
@@ -27,7 +27,7 @@
 ;;; Commentary:
 
 ;; `helm-tail' command displays contents of some special buffers.
-;; Use C-c TAB or C-c C-y to insert/store a selection.
+;; Use C-c TAB or C-c C-k to insert/store a selection.
 
 ;;; Code:
 
